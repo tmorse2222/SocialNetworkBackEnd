a30107f6b5b487cdc75810d38cf44e295e08dc3e
Tyler M <tmorse2222@gmail.com>
2022-10-25 19:49:37 -0700

    Created models/Reaction.js

---
+++ Develop/models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+module.exports = reactionSchema;
