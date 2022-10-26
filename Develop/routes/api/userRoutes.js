d9bc1a9f70062f246e324a537096ce075240d6fa
Tyler M <tmorse2222@gmail.com>
2022-10-25 19:37:52 -0700

    Added friend route handler to routes/api/userRoutes.js

--- Develop/routes/api/userRoutes.js
+++ Develop/routes/api/userRoutes.js
@@ -5,10 +5,14 @@ const {
     createUser,
     updateUser,
     deleteUser,
+    addFriend,
+    removeFriend
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
