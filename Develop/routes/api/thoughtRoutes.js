eb62f50e0b4705745e1dea779ba1cf57f5129a17
Tyler M <tmorse2222@gmail.com>
2022-10-25 19:35:46 -0700

    Created routes/api/thoughtRoutes.js

---
+++ Develop/routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
